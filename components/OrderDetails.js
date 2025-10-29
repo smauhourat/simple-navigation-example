fb48cc464bf554a14fd8b2a079874b94ad2d16de
Santiago Mauhourat <santiagomauhourat@hotmail.com>
2025-10-29 15:37:34 -0300

    order details from api

--- components/OrderDetails.js
+++ components/OrderDetails.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { View, Text, StyleSheet, Pressable } from "react-native";
 
-export default function OrderDetails({ orderId }) {
+export default function OrderDetails({ order }) {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Order Details orderId: {orderId}</Text>
+            <Text style={styles.text}>Order Details orderId: {order.id}</Text>
         </View>
     )
 }
--- screens/OrderDetailsScreen.js
+++ screens/OrderDetailsScreen.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import OrderDetails from '../components/OrderDetails';
+import { getOrderById } from '../lib/api';
 
 const OrderDetailsScreen = ({ route }) => {
     const { orderId } = route.params;
+    const [ order, setOrder ] = useState([]);
+    console.log("OrderDetailsScreen orderId:", orderId);
+
+    useEffect(() => {
+      async function fetchOrderById(id) {
+        const response = await getOrderById(9);
+        console.log("OrderDetailsScreen response:", response);
+        setOrder(response);
+      }
+      fetchOrderById(orderId);
+    }, []);
 
     return (
         <View style={styles.container}>
-            <OrderDetails orderId={orderId} />
+            <OrderDetails order={order} />
         </View>
     );
 };
