8a7ef030f3f865b4c47f92b5375c271e952b9d7d
Santiago Mauhourat <santiagomauhourat@hotmail.com>
2025-11-26 18:35:17 -0300

    fix refresh orders

--- screens/OrdersScreen.js
+++ screens/OrdersScreen.js
@@ -1,18 +1,38 @@
-import { useEffect, useState } from 'react';
-import { View, StyleSheet, FlatList, Button } from 'react-native';
+import { useEffect, useState, useCallback } from 'react';
+import { View, StyleSheet, FlatList, Button, RefreshControl  } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 import OrderCard from '../components/OrderCard';
 import { getOrders } from '../lib/api';
 
 const OrdersScreen = ({ navigation  }) => {
  const [orders, setOrders] = useState([]);
+ const [refreshing, setRefreshing] = useState(false);
 
-  useEffect(() => {
-    async function fetchOrders() {
+    const fetchOrders = async () => {
       const response = await getOrders();
       setOrders(response.data);
     }
-    fetchOrders();
-  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchOrders();
+    }, [])
+  );
+
+  const onRefresh = useCallback(async () => {
+    console.log('Refreshing orders...');
+    setRefreshing(true);
+    await fetchOrders();
+    setRefreshing(false);
+  }, []);  
+
+  // useEffect(() => {
+  //   async function fetchOrders() {
+  //     const response = await getOrders();
+  //     setOrders(response.data);
+  //   }
+  //   fetchOrders();
+  // }, []);
 
   return (
     <View style={styles.container}>
@@ -20,7 +40,11 @@ const OrdersScreen = ({ navigation  }) => {
         style={{ width: '100%' }}
         data={orders}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <OrderCard key={item.id} order={item} />}
+        renderItem={({ item }) => <OrderCard key={item.id} order={item} 
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }        
+        />}
       />
       {/* <Button title="Stack" onPress={() => navigation.navigate("StackScreen")} /> */}
     </View>
